9cf3fb5049cd9ebdd81e6e424f24537fcebea92b
LargeCrowd <102574520+LargeCrowd@users.noreply.github.com>
2024-09-23 16:51:09 +0800

    Implement Jianfeng's Dashboard code & install dependencies

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ViewPage from '@/views/ViewPage.vue'
+import Dashboard from '../components/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
       path: "/",
       name: "Dashboard",
       component: ViewPage,
+    },
+
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
     },
 
     {
