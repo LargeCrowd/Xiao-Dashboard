67972665ede9813b5fde659e303228f74071459c
LargeCrowd <102574520+LargeCrowd@users.noreply.github.com>
2024-09-24 09:46:34 +0800

    Update image-to-text.py

--- image-to-text.py
+++ image-to-text.py
@@ -7,27 +7,36 @@ load_dotenv()
 IMAGE_PATH = "google-image.png"
 MODEL = "gpt-4o-mini"
 
+client = OpenAI()
+
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
-base64_image = encode_image(IMAGE_PATH)
-# print(base64_image)
 
-client = OpenAI()
 
-response = client.chat.completions.create(
-    model=MODEL,
-    messages=[
-        {"role": "user", "content": [
-            {"type": "text", "text": "describe the image"},  # Set the prompt
-            {"type": "image_url", "image_url": {
-                "url": f"data:image/png;base64,{base64_image}"
+def get_image_description(image_path):
+    base64_image = encode_image(image_path)
+    # print(base64_image)
+
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "user", "content": [
+                {"type": "text", "text": "describe the image"},  # Set the prompt
+                {"type": "image_url", "image_url": {
+                    "url": f"data:image/png;base64,{base64_image}"
+                    }
                 }
-            }
-        ]}
-    ],
-    temperature=0.0,
-)
+            ]}
+        ],
+        temperature=0.0,
+    )
 
-print(response.choices[0].message.content)
+    image_description = response.choices[0].message.content
+
+    print(image_description)
+    return image_description
+
+if __name__ == "__main__":
+    get_image_description(IMAGE_PATH)
